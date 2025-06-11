72284b0822b02da083ac183e588b7ab4e6a70b73
riccardogobbo17 <riccardogobbo17@gmail.com>
2025-06-11 18:02:01 +0200

    supabase connection

--- 420mood/pages/1_partite.py
+++ 420mood/pages/1_partite.py
@@ -4,8 +4,8 @@ import numpy as np
 from supabase import create_client, Client
 
 # === CONFIGURAZIONE SUPABASE ===
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+SUPABASE_URL = "https://jinmmonxjovoccejgwhk.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imppbm1tb254am92b2NjZWpnd2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NTk3NzksImV4cCI6MjA2NTEzNTc3OX0.kapMvgvW-6fng-RhxZV_YFcLnxcXo9Bg2wpDWu_H-5g"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # === FUNZIONI SUPPORTO ===
