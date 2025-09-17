b5d9e514590aba69de716b3db1d437305bc380c3
riccardogobbo17 <riccardogobbo17@gmail.com>
2025-09-17 14:41:53 +0200

    fix

--- app/Home.py
+++ app/Home.py
@@ -3,4 +3,4 @@ st.set_page_config(page_title="Home", page_icon="🏠", layout="centered")
 st.markdown("""
 ⬅️ MENU 
 """)
-st.image("../app/imgs/logoFMP.png", caption="", use_container_width=True)
+# st.image("../app/imgs/logoFMP.png", caption="", use_container_width=True)
